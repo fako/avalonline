ae10f7b66a659f552241a01d82110bf0138b723d
Fako Berkers <email@fakoberkers.nl>
2016-07-09 16:24:43 +0200

    Fix player/game relationship.

--- avalonline/models/game.py
+++ avalonline/models/game.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Game(models.Model):
     qr_code = models.ImageField()
+    game_master = models.ForeignKey('Player', related_name="+", null=True, blank=True)
--- avalonline/views/game.py
+++ avalonline/views/game.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, RequestContext, Http404
 from django.views.generic import TemplateView
 
-from avalonline.models.game import Game
+from avalonline.models import Game, Player
 
 class GameView(TemplateView):
     template_name = "start_avalonline.html"
@@ -16,6 +16,18 @@ class GameView(TemplateView):
         return render_to_response(self.template_name, {"game": game}, RequestContext(request))
 
     def post(self, request, *args, **kwargs):
+
         # TODO: create QR code and add to game
         game = Game.object.create(qr_code="")
-        return render_to_response(self.template_name, {"game": game}, RequestContext(request))
+        player = Player.objects.create(game=game, nickname=request.POST.get("nickname"))
+        game.game_master = player
+        game.save()
+        return render_to_response(self.template_name, {"game": game, "player": player}, RequestContext(request))
+
+
+def start_game(request, game_id):
+    try:
+        game = Game.objects.get(id=game_id)
+    except Game.DoesNotExist:
+        raise Http404('Game {} does not exist'.format(game_id))
+    return render_to_response("start.html", {"game": game}, RequestContext(request))
